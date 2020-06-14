6dbb7ea31d985c7d54d735054ffa842d7e90d81e
MithraicMagic <nickelbink@gmail.com>
2020-06-15 00:09:15 +0200

    - fixed links

--- src/Pages/Admin/AdminFetches.js
+++ src/Pages/Admin/AdminFetches.js
@@ -5,10 +5,10 @@ export default class AdminFetches {
         let screenings;
         let movies;
 
-        await fetch('https:/ikhoudvanfilms.com/api/cinemas/all').then(res => res.json()).then(res => cinemas = res);
-        await fetch('https:/ikhoudvanfilms.com/api/theaters/all').then(res => res.json()).then(res => theaters = res);
-        await fetch('https:/ikhoudvanfilms.com/api/screenings/all').then(res => res.json()).then(res => screenings = res);
-        await fetch('https:/ikhoudvanfilms.com/api/movies/all').then(res => res.json()).then(res => movies = res);
+        await fetch('https://ikhoudvanfilms.com/api/cinemas/all').then(res => res.json()).then(res => cinemas = res);
+        await fetch('https://ikhoudvanfilms.com/api/theaters/all').then(res => res.json()).then(res => theaters = res);
+        await fetch('https://ikhoudvanfilms.com/api/screenings/all').then(res => res.json()).then(res => screenings = res);
+        await fetch('https://ikhoudvanfilms.com/api/movies/all').then(res => res.json()).then(res => movies = res);
 
         return {cinemas, theaters, screenings, movies}
     }
